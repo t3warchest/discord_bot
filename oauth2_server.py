18031a94f7c7901c591fdaafb54625bf372e202a
Aruj Jain <cloakacnt@gmail.com>
2023-12-16 17:53:46 +0530

    port update 2

--- oauth2_server.py
+++ oauth2_server.py
@@ -77,4 +77,4 @@ def callback():
         return redirect(authorisation_url) 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=5000)
