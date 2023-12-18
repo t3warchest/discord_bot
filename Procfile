9c37d0720b55fb0b1c84a5a744b0b8135054eebe
Aruj Jain <cloakacnt@gmail.com>
2023-12-18 16:16:44 +0530

    p u 4

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn oauth2_server:app --host 0.0.0.0 --port $PORT
+web: python oauth2_server.py
--- oauth2_server.py
+++ oauth2_server.py
@@ -76,5 +76,7 @@ def callback():
         #redirect back to authorisation url
         return redirect(authorisation_url) 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    
+    app.run(host='0.0.0.0', port=port)
